8e67b772bb17356611b665e327457323772d8a86
gregoryquick <gregory@quicks.me>
2016-11-10 20:14:48 -0800

    #7 moved math util
    Moved math util to utilities package

--- src/org/usfirst/frc/team948/utilities/Mathutil.java
+++ src/org/usfirst/frc/team948/utilities/Mathutil.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team948.utilities;
 
 public class Mathutil {
-
+	public static int clamp(double input, double min, double max){
+		if(input < min){
+			return min;
+		}
+		else if(input > max){
+			return max;
+		}
+		return input;
 }
