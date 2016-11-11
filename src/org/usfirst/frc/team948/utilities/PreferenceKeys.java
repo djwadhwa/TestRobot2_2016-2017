5519d20e79bae65fcaf85d3fd53d3c7398fac3f6
IntOwl <michellethedoctor@gmail.com>
2016-11-10 20:17:19 -0800

    Added PreferenceKeys
    Added class PreferenceKeys

---
+++ src/org/usfirst/frc/team948/utilities/PreferenceKeys.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team948.utilities;
+
+public class PreferenceKeys {
+	public static final String Turn_P = "TurnP";
+	public static final String Turn_I = "TurnI";
+	public static final String Turn_D = "TurnD";
+	public static final String Drive_Straight_On_Heading_P = "DriveStraightP";
+	public static final String Drive_Straight_On_Heading_I = "DriveStraightI";
+	public static final String Drive_Straight_On_Heading_D = "DriveStraightD";
+}
