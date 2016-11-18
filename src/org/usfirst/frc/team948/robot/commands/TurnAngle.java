9ebfc0bcc52124354250c5698a736e8e5ec92e73
Isabel O <kidlatmc@gmail.com>
2016-11-17 19:36:06 -0800

    TurnAngle Class INCOMPLETE
    -Made TurnAngle class in command's package
    -Added three variables (angle, power, and tolerance)
    -Implemented methods from command package

---
+++ src/org/usfirst/frc/team948/robot/commands/TurnAngle.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team948.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TurnAngle extends CommandBase {
+
+	private double power;
+	private double angle; 
+	private double tolerance; 
+	
+	
+	protected void initialize() {
+		
+	}
+
+	
+	protected void execute() {
+		
+	}
+
+	
+	protected boolean isFinished() {
+		return false;
+	}
+	
+
+	protected void end() {
+		
+	}
+	
+	
+	protected void interrupted() {
+		
+		
+	}
+
+}
